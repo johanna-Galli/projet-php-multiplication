20791aa4dc8935db03749abbd0576db0ef1f9b80
Johanna Galli <galli.johanna.g2@gmail.com>
2020-01-10 15:26:31 +0100

    insertion tous les elements page index

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!--HEADER-->
+<!------------------------HEADER---------------------------->
 
 <?php
 $title = "index";
@@ -8,18 +8,37 @@ include "include/header.php";
 
 
 <div class="container">
-    <div class="row">
-        <div class="d-inline col-6 mx-auto">
-            <a href="page2">Page2</a>
+
+    <!---------------MESSAGE DE BIENVENU-------------------->
+
+    <h5 class="text-center">Bonjour à tous et soyez les bienvenus !</h5>
+
+    <p class="mt-3 mb-5 pb-5 text-center">Nous vous proposons aujourd'hui d'apprendre vos tables de multiplications, et de vous tester.</p>
+
+
+    <!----------------------BOUTONS------------------------->
+
+    <div class="row mt-5">
+
+        <div class="d-inline col-6 mt-0">
+            <div class="w-75 h-100 bg-dark mx-auto">
+                <a href="page2">
+                    <h6 class="text-center text-white p-5 bouton m-0">Apprendre les tables de multiplication</h6>
+                </a>
+            </div>
         </div>
 
-        <div class="d-inline col-6">
-            <a href="page3">Page3</a>
+        <div class="d-inline col-6 mt-0">
+            <div class="w-75 h-100 bg-dark mx-auto">
+                <a href="page3">
+                    <h6 class="text-center text-white p-5 bouton m-0">Testez vos connaissances</h6>
+                </a>
+            </div>
         </div>
     </div>
 </div>
 
 
-<!--FOOTER-->
+<!-------------------------FOOTER--------------------------->
 <?php
 include "include/footer.php";
