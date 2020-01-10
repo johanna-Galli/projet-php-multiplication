de2ee8378a14da239668872b92909adbe57fde8c
Johanna Galli <galli.johanna.g2@gmail.com>
2020-01-10 11:21:18 +0100

    header + ajout fichier css + debut changement bouton page accueil

--- include/header.php
+++ include/header.php
@@ -6,7 +6,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?php echo $title ?></title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 </head>
 
 <body>
 
+
+    <!--HEADER-->
+
+    <div class="shadow-sm p-3 mb-5 rounded bg-dark">
+        <a href="index">
+            <h1 class="text-white text-center">
+                Les tables de multiplication
+            </h1>
+        </a>
+    </div>
--- index.php
+++ index.php
@@ -1,13 +1,22 @@
 <!--HEADER-->
-    
+
 <?php
 $title = "index";
 
 include "include/header.php";
 ?>
 
-<a href="page2">Page2</a>
-<a href="page3">Page3</a>
+<div class="container">
+    <div class="row">
+        <div class="d-inline col-6">
+            <a href="page2">Page2</a>
+        </div>
+
+        <div class="d-inline col-6">
+            <a href="page3" class="bouton">Page3</a>
+        </div>
+    </div>
+</div>
 
 
 <!--FOOTER-->
