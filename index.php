9262a6fea9e5495b23c8913bbdda751bfb09f956
Johanna Galli <galli.johanna.g2@gmail.com>
2020-01-10 10:38:05 +0100

    initialisation

---
+++ index.php
@@ -0,0 +1,15 @@
+<!--HEADER-->
+    
+<?php
+$title = "index";
+
+include "include/header.php";
+?>
+
+<a href="page2">Page2</a>
+<a href="page3">Page3</a>
+
+
+<!--FOOTER-->
+<?php
+include "include/footer.php";
---
+++ page3.php
@@ -0,0 +1,15 @@
+<!--HEADER-->
+
+<?php
+$title = "page3";
+
+include "include/header.php";
+?>
+
+<a href="page2">Page2</a>
+<a href="page3">Page3</a>
+
+
+<!--FOOTER-->
+<?php
+include "include/footer.php";
