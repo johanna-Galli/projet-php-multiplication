0b5263ed208b330de3b6272b3d4eafa8cfd2ed52
Johanna Galli <galli.johanna.g2@gmail.com>
2020-01-10 12:01:42 +0100

    maj

--- include/header.php
+++ include/header.php
@@ -5,8 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?php echo $title ?></title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <title><?php echo $title ?></title> 
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="asset/style/style.css">
+
 </head>
 
 <body>
@@ -14,7 +16,7 @@
 
     <!--HEADER-->
 
-    <div class="shadow-sm p-3 mb-5 rounded bg-dark">
+    <div class="p-3 mb-5 rounded-bottom bg-dark">
         <a href="index">
             <h1 class="text-white text-center">
                 Les tables de multiplication
--- index.php
+++ index.php
@@ -6,14 +6,15 @@ $title = "index";
 include "include/header.php";
 ?>
 
+
 <div class="container">
     <div class="row">
-        <div class="d-inline col-6">
+        <div class="d-inline col-6 mx-auto">
             <a href="page2">Page2</a>
         </div>
 
         <div class="d-inline col-6">
-            <a href="page3" class="bouton">Page3</a>
+            <a href="page3">Page3</a>
         </div>
     </div>
 </div>
