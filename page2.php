9bb83f1b0550ffcb9d9a232755d7c9571113744f
Johanna Galli <galli.johanna.g2@gmail.com>
2020-01-13 11:16:23 +0100

    ajout de la fonction qui effectue les tables de multiplications

--- page2.php
+++ page2.php
@@ -12,7 +12,7 @@ include "include/header.php";
 ?>
 
 <form action="page2" method="post">
-    <fieldset >
+    <fieldset>
         <legend class="text-center p-2">Sélectionnez les tables que vous souhaitez apprendre :</legend>
         <div class="text-center p-3">
             <div class="d-inline p-3">
@@ -64,57 +64,72 @@ include "include/header.php";
 </form>
 
 
+<div>
 
 <?php
 
 
-if (isset($_POST['table1'])){
-    echo "table1";
+if (isset($_POST['table1'])) {
+    table(1);
 }
-if (isset($_POST['table2'])){
-    echo "table2";
+
+if (isset($_POST['table2'])) {
+    table(2);
 }
-if (isset($_POST['table3'])){
-    echo "table3";
+
+if (isset($_POST['table3'])) {
+    table(3);
 }
-if (isset($_POST['table4'])){
-    echo "table4";
+
+if (isset($_POST['table4'])) {
+    table(4);
 }
-if (isset($_POST['table5'])){
-    echo "table5";
+
+if (isset($_POST['table5'])) {
+    table(5);
 }
-if (isset($_POST['table6'])){
-    echo "table6";
+
+if (isset($_POST['table6'])) {
+    table(6);
 }
-if (isset($_POST['table7'])){
-    echo "table7";
+
+if (isset($_POST['table7'])) {
+    table(7);
 }
-if (isset($_POST['table8'])){
-    echo "table8";
+
+if (isset($_POST['table8'])) {
+    table(8);
 }
-if (isset($_POST['table9'])){
-    echo "table9";
+
+if (isset($_POST['table9'])) {
+    table(9);
 }
-if (isset($_POST['table10'])){
-    echo "table10";
+
+if (isset($_POST['table10'])) {
+    table(10);
 }
 
 
-
-
-$un = 1;
-$deux = 2;
-
-for ($a = 1; $a <= 10; $a++) {
-    $facteur = $a;
-    for ($b = 1; $b <= 10; $b++) {
-        $multiple = $b;
-        $result = $facteur * $multiple;
-        //echo $result;
+function table($facteur)
+{
+    echo '<span class="table">';
+    echo "Table de $facteur : <br>";
+    for ($a = 1; $a <= 10; $a++) {
+        $result = $facteur * $a;
+        $tableau = array($a => "$facteur x $a = $result");
+        foreach ($tableau as $value) {
+            echo $value . "<br>";
+        }
     }
-    echo "<br>";
+    echo '</span>';
+
 }
 
+?>
+
+</div>
+
+<?php
 
 
 //<!--FOOTER-->
